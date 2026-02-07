7368c2d0b77fb61e335dc52bf9283e757fac0a28
Manish Goyal <mgoyal05.mg@gmail.com>
2026-02-07 23:51:37 +0530

    Accept userHandle in attestation payload

--- src/main/java/com/tara/passkey/webauthn/WebAuthnAttestationRequest.java
+++ src/main/java/com/tara/passkey/webauthn/WebAuthnAttestationRequest.java
@@ -3,6 +3,6 @@ package com.tara.passkey.webauthn;
 import java.util.List;
 
 public record WebAuthnAttestationRequest(String id, String rawId, String type, AttestationResponse response, List<String> transports) {
-    public record AttestationResponse(String clientDataJSON, String attestationObject) {
+    public record AttestationResponse(String clientDataJSON, String attestationObject, String userHandle) {
     }
 }
